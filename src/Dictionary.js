ce78745497b2628f919d3c174b23de7aaca53b85
Vivian Nguyen <vivianyn.co@gmail.com>
2024-08-22 15:01:13 -0400

    Render the word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    setResults(response.data);
   }
 
   function search(event) {
     event.preventDefault();
     const apiKey = "1cb183b1ed5397d3f0a3d9odtb9c94bc";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -26,10 +27,11 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input
           type="search"
-          autoFocus="true"
+          autoFocus={true}
           onChange={handleKeywordChange}
         ></input>
       </form>
+      <Results results={results} />
     </div>
   );
 }
