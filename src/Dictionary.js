e9b9be0ffb8370c2af42b0f1c201afc6d8f09d9d
Vivian Nguyen <vivianyn.co@gmail.com>
2024-08-22 13:49:29 -0400

    Add dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    const apiKey = "1cb183b1ed5397d3f0a3d9odtb9c94bc";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
